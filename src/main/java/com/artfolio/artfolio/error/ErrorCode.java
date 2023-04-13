e7c1bb8d36b08360ce447597d4424b0ce8566baf
황승수 (Seungsu) <h970126@gmail.com>
2023-04-13 14:50:09 +0900

    #5 - 예외 처리

---
+++ src/main/java/com/artfolio/artfolio/error/ErrorCode.java
@@ -0,0 +1,20 @@
+package com.artfolio.artfolio.error;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+    INPUT_VALUE_INVALID("INPUT_VALUE_INVALID", "데이터 형식이 맞지 않습니다.", 400),
+    AUCTION_NOT_FOUND("AUCTION_NOT_FOUND", "해당 경매를 찾을 수 없습니다.", 400)
+    ;
+
+    private final String code;
+    private final String message;
+    private final Integer status;
+
+    ErrorCode(String code, String message, Integer status) {
+        this.code = code;
+        this.message = message;
+        this.status = status;
+    }
+}
---
+++ src/main/java/com/artfolio/artfolio/error/ExceptionHandlingController.java
@@ -0,0 +1,64 @@
+package com.artfolio.artfolio.error;
+
+import com.artfolio.artfolio.exception.AuctionNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestControllerAdvice
+public class ExceptionHandlingController {
+    /* validation 검증 실패시 발생하는 예외 핸들링 메서드 */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("프론트로부터 잘못된 요청 값이 전달됨");
+        List<ErrorResponse.FieldError> fieldErrors = getFieldErrors(e.getBindingResult());
+        return buildFieldErrors(ErrorCode.INPUT_VALUE_INVALID, fieldErrors);
+    }
+
+    /* 존재하지 않는 경매 번호가 넘어왔을 때 예외 핸들링 메서드 */
+    @ExceptionHandler(AuctionNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ErrorResponse handleAuctionNotFoundException(AuctionNotFoundException e) {
+        log.error("프론트로부터 잘못된 경매 번호가 전달됨");
+        return buildError(ErrorCode.AUCTION_NOT_FOUND);
+    }
+
+    private List<ErrorResponse.FieldError> getFieldErrors(BindingResult binding) {
+        return binding.getFieldErrors()
+                .parallelStream()
+                .map(err -> new ErrorResponse.FieldError(
+                        err.getField(),
+                        (String) err.getRejectedValue(),
+                        err.getDefaultMessage()
+                ))
+                .collect(Collectors.toList());
+    }
+
+    private ErrorResponse buildFieldErrors(ErrorCode errorCode, List<ErrorResponse.FieldError> fieldErrors) {
+        return new ErrorResponse(
+                errorCode.getCode(),
+                errorCode.getCode(),
+                errorCode.getStatus(),
+                fieldErrors
+        );
+    }
+
+    private ErrorResponse buildError(ErrorCode errorCode) {
+        return new ErrorResponse(
+                errorCode.getCode(),
+                errorCode.getMessage(),
+                errorCode.getStatus(),
+                List.of()
+        );
+    }
+}
